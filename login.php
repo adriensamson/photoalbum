11c743731407119835c186aa0038c4a05d27fd66
Adrien Samson <adrien@kyklydse.com>
2008-12-13 23:30:19 +0000

    login par email et oubli de mot de passe

--- login.php
+++ login.php
@@ -66,6 +66,8 @@ http://www.getfirefox.com
 	$subject = "Oubli de votre mot de passe sur photoalbum";
 	$header='Content-Type: text/plain; charset="UTF-8"\r\n';
 	mail($email, $subject, $message, $header, '-f photoalbum@kyklydse.com');
+	$url = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+	header("Location: $url");
 	}
 }
 else
